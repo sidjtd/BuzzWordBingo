ef79df7470fa3424d1dff2fe4a1ef55e5d771530
SDodson <siddodson@gmail.com>
2016-07-13 12:14:00 -1000

    Refactor PUT, DELETE completed, only have POST reset left

--- bingo.js
+++ bingo.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-Parser');
 var app = express();
 
 //storage
-var buzzWords = {};
+var buzzWordHolder = {};
+var arr =[];
 var PORT = 4000;
 /*                ROUTES
  ____________________________________________________
@@ -18,53 +19,76 @@ var PORT = 4000;
  ____________________________________________________
  ----------------------------------------------------  */
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
 
  /*              EXPRESS
  ____________________________________________________
  ----------------------------------------------------  */
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true}));
-
-// // app.use('/buzzword', buzzwordRouter);
-
-var arr =[];
-var buzzWords = {};
-
-app.get('/', (req, res) =>{
-  res.send('./public/');
-});
-
-app.get('/buzzwords', (req, res) => {
-  res.send( { "buzzwords": arr});
-//res.json(buzzWords);
-});
-
-app.post('/buzzwords', (req, res) => {
-   var body = req.body;
-  if(typeof body.buzzWord === 'string' && body.buzzWord && typeof Number(body.points) === 'number' && body.points) {
-    buzzWords[body.buzzWord] = body;
-    return res.json({"success" : true});
-  }
-  return res.json({"success" : false});
-});
-
-
-var server = app.listen(PORT, function (){
-  var host = server.address().address;
-  var port = server.address().port;
-  //console.log(`Example app listening at :localhost:${port}`);
-});
-
-// server---<<<
-/*___________________________________________________
-  --------------------------------------------------- */
-
+// app.use('/buzzword', buzzwordRouter);
+// app.use(bodyParser.json());ß
 
 //  __    __ _____  _____ \_ __    _____  _____  __ __  __ _____
 //  \\ /\ //((   )) ||_// ||<<     ||_// ((   )) || ||\\||  ||
 //   \V/\V/  \\_//  || \\ || \\    ||     \\_//  || || \||  ||
 
+//app.put('/buzzwords', upload.array(), function (req, res) {
+app.put('/buzzwords', function (req, res) {
+  var body = req.body;
+
+  if(typeof body.buzzWords === 'string' && body.buzzWords && typeof Boolean(body.heard) === 'boolean' && body.heard) {
+    if(buzzWordHolder.hasOwnProperty(body.buzzWords)) {
+      buzzWordHolder[body.buzzWords].heard = body.heard;
+      return res.json({"success" : true});
+    }
+  }
+  return res.json({"success" : false});
+});
+
+app.delete('/buzzWords', function (req, res) {
+  var body = req.body;
+  if(buzzWordHolder.hasOwnProperty(body.buzzWords)) {
+      delete buzzWordHolder[body.buzzWords];
+      return res.json({"success" : true});
+    }
+  return res.json({"success" : false});
+});
+
+app.post('/reset', function (req, res) {
+
+});
+
 /*  ----------------------------------------------------------------------------
    / ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★   COMPLETED!  ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★  \
    ______________________________________________________________________________
    ------------------------------------------------------------------------------  */
+
+/*  app.get
+ --------------*/
+app.get('/', (req, res) =>{
+  res.send('./public/');
+});
+
+app.get('/buzzwords', (req, res) => {
+  res.send(buzzWords);
+//res.json(buzzWords);
+});
+
+ /*  app.post
+ --------------*/
+// app.post('/buzzwords', upload.array(), (req, res) => {
+app.post('/buzzwords', (req, res) => {
+   var body = req.body;
+  if(typeof body.buzzWords === 'string' && body.buzzWords && typeof Number(body.points) === 'number' && body.points) {
+    buzzWordHolder[body.buzzWords] = body;
+    return res.json({"success" : true});
+  }
+  return res.json({"success" : false});
+});
+
+ /*  server
+ --------------*/
+var server = app.listen(PORT, function (){
+  var host = server.address().address;
+  var port = server.address().port;
+  //console.log(`Example app listening at :localhost:${port}`);
+}); // server---<<<
